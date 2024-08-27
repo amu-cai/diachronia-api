0b2965cb3fe6cc95667b77816948f7653334c114
Filip Patyk <fp.patyk@gmail.com>
2024-08-27 14:48:13 +0200

    feat: add file arguemnt to cli input

--- src/dariah/__main__.py
+++ src/dariah/__main__.py
@@ -6,12 +6,17 @@ parser = ArgumentParser(description="DARIAH CLI tool")
 
 
 parser.add_argument("mode", type=str, choices=["dating", "diachronic_normalizer", "disambiguation", "synonyms"])
-parser.add_argument("text", type=str)
+parser.add_argument("text", type=str, nargs="?", default=" ")
 parser.add_argument("--output", "-o", type=str)
+parser.add_argument("--file", "-f", type=str)
+
 ArgumentParser
 args = parser.parse_args()
 
 if __name__ == "__main__":
+    if args.file:
+        with open(args.file, "r") as f:
+            text = f.read()
     match args.mode:
         case "dating":
             result = dating(args.text)
